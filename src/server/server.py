44a9ed8fbfab2a0bda47b2f7a2b40c37d0c04369
Alan Devine <alan.devine24@mail.dcu.ie>
2020-05-07 15:19:57 +0100

    Added config json parsing

--- src/server/server.py
+++ src/server/server.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 
+import socket
+import json
+
 
 def main():
-    pass
+    with open("./server_config.json") as config_file:
+        config = json.load(config_file)
+        host = config["settings"]["host"]
+        port = config["settings"]["port"]
+
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.bind((host, port))
+    soc.listen(5)
 
 
 if __name__ == "__main__":
