081419fd0a502ffdd80091bf1f79edec5b0f7934
= <alan.devine24@mail.dcu.ie>
2020-06-12 20:12:09 +0100

    client/ server communication working

--- src/client/client.py
+++ src/client/client.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python3
-
-import socket
 import json
+import requests
+import os
+import sys
+import time
+from datetime import datetime
 
 
 class Client:
@@ -21,6 +23,13 @@ class Client:
             self.minimum_amount_ml = config["hardware_settings"]["minimum_amount_ml"]
             self.sanitizer_threshold = config["hardware_settings"]["sanitizer_threshold"]
 
+            self.sensor_path = config["hardware_settings"]["sensor_path"]
+
+        self.sensor_file = f"{os.getcwd()}/src/client/{self.sensor_path}"
+        self.id = 2
+
+        self.msg_backlog = []
+
         debug_str = []
         debug_str.append("[Network Settings]")
         debug_str.append(f"Host : {self.host}")
@@ -32,7 +41,36 @@ class Client:
         debug_str.append("-" * len(max(debug_str, key=len)))
         print("\n".join(debug_str))
 
+    def client_loop(self):
+        i = 1
+        while True:
+            print("run:", i)
+            i += 1
+            if os.path.exists(self.sensor_file):
+                with open(self.sensor_file) as f:
+                    activity = f.read()
+                    dt = datetime.now().isoformat()
+                    msg = json.dumps({"device_id": self.id, "activity": activity, "date_time": dt})
+
+                    url = f"http://{self.host}:{self.port}/api/activity"
+
+                    try:
+                        requests.post(url, msg)
+
+                        if len(self.msg_backlog) > 0:
+                            for old_msg in self.msg_backlog:
+                                requests.post(url, old_msg)
+                            self.msg_backlog.clear()
+
+                    except ConnectionError as e:
+                        print(e)
+                        self.msg_backlog.append(msg)
+
+            time.sleep(5)
+
+
+
 
 if __name__ == '__main__':
-    cli = Client("client_config.json")
-
+    cli = Client("src/client/client_config.json")
+    cli.client_loop()
--- src/server/server.py
+++ src/server/server.py
@@ -1,17 +1,40 @@
-from flask import Flask
+from flask import Flask, request
+from Database import Database as DB
+import json
 
 app = Flask(__name__)
-app.config
+database = DB("test.db")
 
 
-@app.route("/devices", methods=["GET"])
+@app.route("/", methods=["GET"])
+def index():
+    pass
+
+
+@app.route("/api/devices", methods=["GET"])
 def get_all_devices():
     pass
 
 
-@app.route("/devices", methods=["POST"])
+@app.route("/api/devices/<int:device_id>", methods=["GET"])
+def get_device(device_id):
+    pass
+
+
+@app.route("/api/devices", methods=["POST"])
 def add_new_device():
     pass
+
+
+@app.route("/api/activity", methods=["POST"])
+def add_new_entry():
+    msg = json.loads(request.get_data())
+    device_id = msg["device_id"]
+    date_time_iso = msg["date_time"]
+    activity = msg["activity"]
+
+    print(f"{device_id}\t{date_time_iso}\t{activity}")
+    return "success"
 
 
 if __name__ == "__main__":
