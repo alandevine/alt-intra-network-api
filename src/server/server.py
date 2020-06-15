d2ce720999705debcadd1600a4ed325594e5d575
= <alan.devine24@mail.dcu.ie>
2020-06-15 17:35:04 +0100

    Deleted unnecessary files

--- src/server/server.py
+++ src/server/server.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
-from Database import Database as DB
+import os
 import json
+import sqlite3
 
 app = Flask(__name__)
-database = DB("test.db")
+database = "dispenser.db"
 
 
 @app.route("/", methods=["GET"])
@@ -32,10 +33,84 @@ def add_new_entry():
     device_id = msg["device_id"]
     date_time_iso = msg["date_time"]
     activity = msg["activity"]
+    print("------")
 
-    print(f"{device_id}\t{date_time_iso}\t{activity}")
-    return "success"
+    with sqlite3.connect(database) as conn:
+        cursor = conn.cursor()
+        query = """
+                INSERT INTO deviceActivity
+                    (
+                        id, 
+                        dateTime, 
+                        type
+                    )
+                VALUES(?, ?, ?)
+                """
+
+        entry = (device_id, date_time_iso, activity)
+        cursor.execute(query, entry)
+        conn.commit()
+        cursor.close()
+
+    return "Successfully added entry", 201
+
+
+def create_database():
+    """
+    Creates Tables for database
+    """
+
+    with sqlite3.connect(database) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+                CREATE TABLE deviceActivity
+                    (
+                       id       TEXT,
+                       dateTime TEXT,
+                       type     TEXT
+                    )  
+            """
+        )
+
+        cursor.execute(
+            """
+                CREATE TABLE devices
+                  (
+                     id                  TEXT,
+                     dataOfRegistration  TEXT,
+                     timeOfRegistration  TEXT,
+                     location            TEXT,
+                     batteryLevel        FLOAT,
+                     fluidLevel          FLOAT,
+                     dateLastMaintenance TEXT
+                  ) 
+            """
+        )
+
+        conn.commit()
+        cursor.close()
+        print("Created Database with tables:")
+        print("\tdeviceActivity")
+        print("\t\tid                  TEXT")
+        print("\t\tdateTime            TEXT")
+        print("\t\ttype                TEXT")
+        print("\tdevices")
+        print("\t\tid                  TEXT")
+        print("\t\tdataOfRegistration  TEXT")
+        print("\t\ttimeOfRegistration  TEXT")
+        print("\t\tlocation            TEXT")
+        print("\t\tbatteryLevel        FLOAT")
+        print("\t\tfluidLevel          FLOAT")
+        print("\t\tdateLastMaintenance TEXT")
+
+
+def main():
+    if not os.path.exists(database):
+        create_database()
+
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
