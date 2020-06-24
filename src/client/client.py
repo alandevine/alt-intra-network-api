9bfe2d391313754090c5d4be52d5deaeb31466ce
Alan Devine <alan.devine24@mail.dcu.ie>
2020-06-24 12:49:16 +0100

    Implemented client in flask

--- src/client/client.py
+++ src/client/client.py
@@ -1,85 +1,107 @@
 from datetime import datetime
+from flask import Flask
 import json
 import requests
 import socket
+import threading
 
-"""
-    TODO:
-        + write id to json config file
-        + implement settings changes
-"""
+DEVICE_ID = None
+CLIENT_CONFIG_FILE = "client_config.json"
+MSG_BACKLOG = []
+
+app = Flask(__name__)
+
+with open(CLIENT_CONFIG_FILE) as config_file:
+    config = json.load(config_file)
+
+    # network settings
+    host = config["network_settings"]["host"]
+    port = config["network_settings"]["port"]
+
+    # hardware settings
+    dispense_amount_ml = config["hardware_settings"]["dispense_amount_ml"]
+    minimum_amount_ml = config["hardware_settings"]["minimum_amount_ml"]
+    sanitizer_threshold = config["hardware_settings"]["sanitizer_threshold"]
+
+    sensor_file = config["hardware_settings"]["sensor_path"]
+
+IP = socket.gethostbyname(socket.gethostname())
+ID = requests.post(f"http://{host}:{port}/api/devices", json.dumps({"ip": IP})).text
+print(f"Device ID: {ID}")
+
+# this should be read from /dev/battery
+BATTERY_LEVEL = 100
+FLUID_LEVEL = 100
 
 
-class Client:
+@app.route("/config/dispense_amount_ml/<float:val>", methods=["POST"])
+def set_dispense_amount(val):
+    update_config("dispense_amount_ml", val)
 
-    def __init__(self, client_config_file):
-        self.client_config_file = client_config_file
 
-        with open(self.client_config_file) as config_file:
-            config = json.load(config_file)
+@app.route("/device/battery_level", methods=["GET"])
+def get_battery_level():
+    return str(BATTERY_LEVEL), 201
 
-            # network settings
-            self.host = config["network_settings"]["host"]
-            self.port = config["network_settings"]["port"]
 
-            # hardware settings
-            self.dispense_amount_ml = config["hardware_settings"]["dispense_amount_ml"]
-            self.minimum_amount_ml = config["hardware_settings"]["minimum_amount_ml"]
-            self.sanitizer_threshold = config["hardware_settings"]["sanitizer_threshold"]
+@app.route("/device/fluid_level", methods=["GET"])
+def get_fluid_level():
+    return str(FLUID_LEVEL), 201
 
-            self.sensor_path = config["hardware_settings"]["sensor_path"]
 
-        self.sensor_file = f"{self.sensor_path}"
-        self.id = None
+def update_config(key, value):
+    try:
+        config["hardware_settings"][key] = value
+    except ValueError:
+        pass
 
-        self.msg_backlog = []
+    with open(CLIENT_CONFIG_FILE, "w") as conf:
+        conf.write((json.dumps(config, indent=4)))
 
-        debug_str = []
-        debug_str.append("[Network Settings]")
-        debug_str.append(f"Host : {self.host}")
-        debug_str.append(f"Port : {self.port}")
-        debug_str.append("[Hardware Settings]")
-        debug_str.append(f"Dispense Amount (in ml) : {self.dispense_amount_ml}")
-        debug_str.append(f"Minimum_amount (in ml)  : {self.minimum_amount_ml}")
-        debug_str.append(f"Sanitizer Threshold     : {self.sanitizer_threshold * 100}%")
-        debug_str.append("-" * len(max(debug_str, key=len)))
-        print("\n".join(debug_str))
 
-        self.ip = socket.gethostbyname(socket.gethostname())
-        self.id = requests.post(f"http://{self.host}:{self.port}/api/devices", json.dumps({"ip": self.ip})).text
-        print(f"Device ID: {self.id}")
+def client_loop():
 
-    def client_loop(self):
-        while True:
-            # client should not be able to send data before receiving id from server
-            if self.id is None:
+    global FLUID_LEVEL
+
+    while True:
+        # client should not be able to send data before receiving id from server
+        if DEVICE_ID is None:
+            continue
+
+        with open(sensor_file, "w+") as f:
+            activity = f.read()
+            # clear sensor file
+            f.write("")
+            if activity == "":
                 continue
 
-            with open(self.sensor_file, "w+") as f:
-                activity = f.read()
-                # clear sensor file
-                f.write("")
-                if activity == "":
-                    continue
+            if activity == "dispense":
+                FLUID_LEVEL -= dispense_amount_ml
+                # also update env variable
 
-                dt = datetime.now().isoformat()
-                msg = json.dumps({"device_id": self.id, "activity": activity, "date_time": dt})
+            dt = datetime.now().isoformat()
+            msg = json.dumps({"device_id": DEVICE_ID, "activity": activity, "date_time": dt})
 
-                url = f"http://{self.host}:{self.port}/api/activity"
+            url = f"http://{host}:{port}/api/activity"
 
-                try:
-                    requests.post(url, msg)
+            try:
+                requests.post(url, msg)
 
-                    if len(self.msg_backlog) > 0:
-                        for old_msg in self.msg_backlog:
-                            requests.post(url, old_msg)
-                        self.msg_backlog.clear()
+                if len(MSG_BACKLOG) > 0:
+                    for old_msg in MSG_BACKLOG:
+                        requests.post(url, old_msg)
+                    MSG_BACKLOG.clear()
 
-                except ConnectionError as e:
-                    print(e)
-                    self.msg_backlog.append(msg)
+            except ConnectionError as e:
+                print(e)
+                MSG_BACKLOG.append(msg)
 
 
-if __name__ == '__main__':
-    cli = Client("client_config.json")
-    cli.client_loop()
+def main():
+    loop = threading.Thread(target=client_loop)
+    loop.start()
+    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    main()
